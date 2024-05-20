e1a43ea453e891fdbb11aa12e61974f3a1f622fc
Valdineide Schunk <valdineideschunk@gmail.com>
2024-05-20 19:18:11 -0300

    Start projeto
    Criação do projeto inicial no NetBeans

---
+++ hospeda_facil/src/main/java/com/mycompany/hospeda_facil/Hospeda_facil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospeda_facil;
+
+/**
+ *
+ * @author vschunk
+ */
+public class Hospeda_facil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
